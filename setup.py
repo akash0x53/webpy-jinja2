d5c2aee3d0e30eeeae8d31bb957e372f5fbc8eba
Anand Chitipothu <anandology@gmail.com>
2015-02-25 17:16:08 +0530

    Bump version to 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webpy-jinja2',
-    version="0.1.0",
+    version="0.1.1",
     description="Library to provide Jinja2 templating support for web.py applications.",
     license="BSD",
     install_requires=[
